f65b15ca2e60ff2f39eb156f15fe2fed16f5d30d
TWSecret <163172500+SanjarTurdimurodov@users.noreply.github.com>
2024-03-12 23:04:03 +0300

    Ilk kod
    Ilk kodimiz kiritildi

---
+++ Ma'lumot beuvchi kod.py
@@ -0,0 +1,17 @@
+
+ism=input("Ismingizni kiriting.\n >>>   ")
+familiya=input("Familiyangizni kiriting.\n>>>   ")
+kocha=input("Ko'changizni kiriting.\n>>>   ")
+mahalla=input("Mahallangizni kiriting.\n>>>   ")
+tuman=input("Tumaningizni kiriting.\n>>>   ")
+viloyat=input("Viloyatingizni kiritong.\n>>>   ")
+
+
+malumot=f"Men {ism} {familiya} {viloyat} viloyati {tuman} tumani {mahalla} mahallasi {kocha} ko'chasida yashayman."
+
+print(malumot)
+
+
+
+
+
